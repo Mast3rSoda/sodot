cdeb18be9739a5b282adb77b5bbffc74e370a775
Mast3rSoda <mast3rsoda@gmail.com>
2025-10-26 18:29:06 +0100

    test new ssh-agent settings

--- .config/zsh/settings/ssh.zsh
+++ .config/zsh/settings/ssh.zsh
@@ -1,12 +1,23 @@
 sodot_ssh_home="$HOME/.ssh"
-ssh_auth_sock="$sodot_ssh_home/ssh_auth_sock"
+
+# check if systemd enabled
+ssh_sock_loc="systemd/user/sockets.target.wants/ssh-agent.socket"
+if [ ! -S $ssh_sock_loc ]; then
+    ssh_sock_loc="$sodot_ssh_home/ssh_sock_loc"
+else
+    export SSH_AUTH_SOCK=$ssh_sock_loc
+fi
 
 # load the agent
-if [ ! -S $ssh_auth_sock ]; then
+# BUG: somehow once the file was created
+# and I couldn't overwrite it. Happened after
+# a forced restart. Eval was ran, but connection error.
+# # Removing the link helped
+if [ ! -S $ssh_sock_loc ]; then
   eval `ssh-agent -s`
-  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+  ln -sf "$SSH_AUTH_SOCK" $ssh_sock_loc
 fi
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+export SSH_AUTH_SOCK=$ssh_sock_loc
 
 # manually add secret keys
 if ! ssh-add -l>/dev/null; then
