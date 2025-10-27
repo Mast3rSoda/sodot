6d8f28401b043fdcac230ef17c90250c6d292079
Mast3rSoda <mast3rsoda@gmail.com>
2025-10-27 06:47:36 +0100

    idk

--- .config/zsh/settings/enable-uwsm.zsh
+++ .config/zsh/settings/enable-uwsm.zsh
@@ -1,7 +1,5 @@
-if [[ -z $DISPLAY ]]; then
-	if uwsm check may-start && uwsm select; then
-		exec uwsm start default
-	fi
-fi
-
-
+# if [[ -z $DISPLAY ]]; then
+# 	if uwsm check may-start && uwsm select; then
+# 		exec uwsm start default
+# 	fi
+# fi
--- .config/zsh/settings/ssh.zsh
+++ .config/zsh/settings/ssh.zsh
@@ -1,28 +1,30 @@
 sodot_ssh_home="$HOME/.ssh"
 
 # check if systemd enabled
-ssh_sock_loc="systemd/user/sockets.target.wants/ssh-agent.socket"
-if [ ! -S $ssh_sock_loc ]; then
-    ssh_sock_loc="$sodot_ssh_home/ssh_sock_loc"
-else
+ssh_sock_loc="$XDG_RUNTIME_DIR/ssh-agent.socket"
+if [ -S $ssh_sock_loc ]; then
     export SSH_AUTH_SOCK=$ssh_sock_loc
+    return
 fi
+
+# ssh_sock_loc="$sodot_ssh_home/ssh_sock_loc"
+
 
 # load the agent
 # BUG: somehow once the file was created
 # and I couldn't overwrite it. Happened after
 # a forced restart. Eval was ran, but connection error.
 # # Removing the link helped
-if [ ! -S $ssh_sock_loc ]; then
-  eval `ssh-agent -s`
-  ln -sf "$SSH_AUTH_SOCK" $ssh_sock_loc
-fi
-export SSH_AUTH_SOCK=$ssh_sock_loc
-
-# manually add secret keys
-if ! ssh-add -l>/dev/null; then
-    for key in $sodot_ssh_home/*.pub
-    do
-        ssh-add ${key%.pub} >/dev/null
-    done
-fi
+# if [ ! -S $ssh_sock_loc ]; then
+#   eval `ssh-agent -s`
+#   ln -sf "$SSH_AUTH_SOCK" $ssh_sock_loc
+# fi
+# export SSH_AUTH_SOCK=$ssh_sock_loc
+#
+# # manually add secret keys
+# if ! ssh-add -l>/dev/null; then
+#     for key in $sodot_ssh_home/*.pub
+#     do
+#         ssh-add ${key%.pub} >/dev/null
+#     done
+# fi
